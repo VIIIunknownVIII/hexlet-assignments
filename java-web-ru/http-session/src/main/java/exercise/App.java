c7364e975bc6d9ae9d60f131c04f63cb43d681ce
@hexlet/cli <support@hexlet.io>
2024-10-08 23:03:02 +0700

    submit java-web-ru/http-session

--- java-web-ru/http-session/src/main/java/exercise/App.java
+++ java-web-ru/http-session/src/main/java/exercise/App.java
@@ -1,6 +1,8 @@
 package exercise;
 
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+
 import java.util.List;
 import java.util.Map;
 
@@ -9,15 +11,16 @@ public final class App {
     private static final List<Map<String, String>> USERS = Data.getUsers();
 
     public static Javalin getApp() {
-
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
 
-        // BEGIN
         app.get("/users", ctx -> {
-            int page = ctx.queryParam("page", "1").equals("") ? 1 : Integer.parseInt(ctx.queryParam("page", "1"));
-            int per = ctx.queryParam("per", "5").equals("") ? 5 : Integer.parseInt(ctx.queryParam("per", "5"));
+            String pageParam = ctx.queryParam("page");
+            String perParam = ctx.queryParam("per");
+
+            int page = (pageParam == null || pageParam.isEmpty()) ? 1 : Integer.parseInt(pageParam);
+            int per = (perParam == null || perParam.isEmpty()) ? 5 : Integer.parseInt(perParam);
 
             int start = (page - 1) * per;
             int end = Math.min(start + per, USERS.size());
@@ -28,12 +31,10 @@ public final class App {
             }
 
             List<Map<String, String>> pagedUsers = USERS.subList(start, end);
-            ctx.json(pagedUsers); // Возвращаем подсписок в формате JSON
+            ctx.json(pagedUsers);
         });
-        // END
 
         return app;
-
     }
 
     public static void main(String[] args) {
