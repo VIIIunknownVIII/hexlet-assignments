f0c85da0c7a1786372cf275788883572408411d6
@hexlet/cli <support@hexlet.io>
2024-10-13 15:01:57 +0700

    submit java-web-ru/mvc

--- java-web-ru/mvc/src/main/java/exercise/App.java
+++ java-web-ru/mvc/src/main/java/exercise/App.java
@@ -24,7 +24,8 @@ public final class App {
         app.get(NamedRoutes.postPath("{id}"), PostsController::show);
 
         // BEGIN
-        
+        app.get(NamedRoutes.editPath("{id}"), PostsController::edit);
+        app.post(NamedRoutes.postPath("{id}"), PostsController::update);
         // END
 
         return app;
